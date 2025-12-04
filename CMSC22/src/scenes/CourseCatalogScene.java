5cd57957a378d77781f344dfa11277f35a9b6dea
Qlyve <bundocandrew119@gmail.com>
2025-12-05 05:18:29 +0800

    fixed repo since was not merging properly
    need to fix more on ui  and important is to not add section if there is a time slot exactly or in conflict

--- CMSC22/src/scenes/CourseCatalogScene.java
+++ CMSC22/src/scenes/CourseCatalogScene.java
@@ -1,6 +1,8 @@
 package scenes;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import application.SceneManager;
 import javafx.collections.FXCollections;
@@ -11,18 +13,22 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Separator;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.control.TableCell;
 import uiandlogic.User;
 import uiandlogic.Course;
+import uiandlogic.Section;
 
 
 public class CourseCatalogScene extends BaseScene {
@@ -35,7 +41,26 @@ public class CourseCatalogScene extends BaseScene {
     
     @Override
     protected Parent buildScene() {
+    	// for debugging
 //    	SceneManager.getDataAccess().viewCourses();
+    	Section sec1 = SceneManager.getDataAccess().findSection("CMSC 123", "C");
+    	Section sec2 = SceneManager.getDataAccess().findSection("CMSC 124", "ST");
+    	Section sec3 = SceneManager.getDataAccess().findSection("CMSC 125", "B-5L");
+    	
+    	
+    	// add sections to user 
+    	if (sec1 != null) {
+			currentUser.planSection(sec1);
+		}
+    	if (sec2 != null) {
+			currentUser.planSection(sec2);
+		}
+    	if (sec3 != null) {
+			currentUser.planSection(sec3);
+		}
+    	
+    	currentUser.viewPlannedCourses();
+    	
     	
     	
     	// get data
@@ -45,7 +70,7 @@ public class CourseCatalogScene extends BaseScene {
         	masterList.addAll(list);
         }
         
-     // main container
+        // main container
     	HBox mainContainer = new HBox();
         // create and add the sidebar
         Sidebar sidebar = new Sidebar(scale, "CourseCatalog", currentUser);
@@ -175,4 +200,117 @@ public class CourseCatalogScene extends BaseScene {
         return mainContainer;
     }
     // METHODS AND FUNCTIONS
+    private VBox buildSection() {
+    	VBox sectionContainer = new VBox();
+    	sectionContainer.setSpacing(10);
+    	sectionContainer.setPadding(new Insets(10));
+    	
+    	// ==Header==
+    	HBox header = new HBox();
+        header.setPadding(new Insets(10));
+        header.setSpacing(40);
+        header.setStyle("-fx-background-color: #293b4d; -fx-background-radius: 5;");
+        
+		Label colCourseCode = new Label("Course Code");
+		Label colCourseTitle = new Label("Course Title");
+		Label colUnits = new Label("Units");
+		Label colSection = new Label("Section");
+		Label colTime = new Label("Time");
+		Label colAction = new Label("Action");
+		
+		List<Label> labels = Arrays.asList(
+		        colCourseCode, colCourseTitle, colUnits, colSection, colTime, colAction
+		);
+
+		for (Label lbl : labels) {
+		    lbl.setTextFill(Color.WHITE);
+		    lbl.setStyle("-fx-font-weight: bold; -fx-font-size: 14;");
+		}
+		
+		HBox codeCol = makeColumn(colCourseCode, 150);
+		HBox titleCol = makeColumn(colCourseTitle, 130);
+		HBox unitsCol = makeColumn(colUnits,  100);
+		HBox sectionCol = makeColumn(colSection, 100);
+		HBox timeCol = makeColumn(colTime, 120);
+	    HBox actionCol  = makeColumn(colAction, 150);
+
+		header.getChildren().addAll(
+				codeCol,
+				titleCol,
+				unitsCol,
+				sectionCol,
+				timeCol,
+				actionCol
+		);
+		
+		header.setPrefWidth(700); 
+		sectionContainer.setAlignment(Pos.CENTER); 
+		
+		sectionContainer.getChildren().add(header);
+		
+		// ==Content Section==
+		VBox dropdownContainer = new VBox();
+	    dropdownContainer.setSpacing(5);
+	    
+	    ArrayList<ObservableList<Course>> listOfLists = SceneManager.getDataAccess().getMasterList();
+        ObservableList<Course> masterList = FXCollections.observableArrayList();
+        for(ObservableList<Course> list: listOfLists) {
+        	masterList.addAll(list);
+        }
+
+	    for (Course course: masterList) {
+	        TitledPane dropdown = new TitledPane();
+
+	        HBox rowHeader = new HBox();
+	        rowHeader.setSpacing(40);
+	        rowHeader.setPadding(new Insets(5));
+
+	        Label dropColCourseCode = new Label(course.getCourseCode());
+	        Label dropColCourseTitle = new Label(course.getCourseName());
+	        Label dropColUnits = new Label(course.getUnits()); 
+	        Label dropColSection = new Label("-");
+	        Label dropColTime = new Label("-");
+	        Label dropColAction = new Label("Action");
+
+	        List<Label> dropLabels = Arrays.asList(dropColCourseCode, dropColCourseTitle, dropColUnits, dropColSection, dropColTime, dropColAction);
+	        for (Label lbl : dropLabels) {
+	            lbl.setTextFill(Color.BLACK);
+	            lbl.setStyle("-fx-font-size: 13;");
+	        }
+
+	        HBox dropCodeCol = makeColumn(dropColCourseCode, 80);
+	        dropCodeCol.setAlignment(Pos.CENTER_LEFT);
+	        HBox dropTitleCol = makeColumn(dropColCourseTitle, 100);
+	        HBox dropUnitsCol = makeColumn(dropColUnits, 50);
+	        HBox dropSectionCol = makeColumn(dropColSection, 70);
+	        HBox dropTimeCol = makeColumn(dropColTime, 67);
+	        HBox dropActionCol = makeColumn(dropColAction, 130);
+
+	        rowHeader.getChildren().addAll(dropCodeCol, dropTitleCol, dropUnitsCol, dropSectionCol, dropTimeCol, dropActionCol);
+
+	        dropdown.setGraphic(rowHeader);
+
+	        Label placeholder = new Label("sections");
+	        placeholder.setPadding(new Insets(10));
+	        dropdown.setContent(placeholder);
+
+	        dropdown.setExpanded(false);
+	        dropdownContainer.getChildren().add(dropdown);
+	    }
+	    
+	    ScrollPane scrollPane = new ScrollPane(dropdownContainer);
+	    scrollPane.setFitToWidth(true);
+	    scrollPane.setPrefHeight(250);
+
+	    sectionContainer.getChildren().add(scrollPane);
+		
+    	return sectionContainer;
+    }
+    
+    private HBox makeColumn(Label label, double width) {
+        HBox box = new HBox(label);
+        box.setAlignment(Pos.CENTER);
+        box.setPrefWidth(width);
+        return box;
+    }
 }
