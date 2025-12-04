c750ca601a778b1e3e15b55e931c0ba74f23771f
ncilocto-ui <ncilocto@up.edu.ph>
2025-12-04 19:53:34 +0800

    added drop down and show pass working

--- CMSC22/src/application/controller/SignUpController.java
+++ CMSC22/src/application/controller/SignUpController.java
@@ -1,59 +1,74 @@
 package application.controller;
 
 import application.SceneManager;
-import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import uiandlogic.User;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
 public class SignUpController {
     
-	// added @FXML to all attributes
-	@FXML private TextField emailField;
-	@FXML private TextField firstNameField;
-	@FXML private TextField middleNameField;
-	@FXML private TextField lastNameField;
-	@FXML private TextField categoryField;
-	@FXML private PasswordField passwordField;
-	@FXML private PasswordField confirmPasswordField;
-	@FXML private CheckBox showPasswordCheck;
+    @FXML private TextField emailField;
+    @FXML private TextField firstNameField;
+    @FXML private TextField middleNameField;
+    @FXML private TextField lastNameField;
 
+    @FXML private ComboBox<String> categoryCombo;
+
+    @FXML private PasswordField passwordField;
+    @FXML private TextField passwordVisibleField;
+
+    @FXML private PasswordField confirmPasswordField;
+    @FXML private TextField confirmPasswordVisibleField;
+
+    @FXML private CheckBox showPasswordCheck;
+
+    @FXML
     public void initialize() {
         System.out.println("SignUpController initialized!");
+
+        passwordVisibleField.textProperty().bindBidirectional(passwordField.textProperty());
+        confirmPasswordVisibleField.textProperty().bindBidirectional(confirmPasswordField.textProperty());
     }
-    
-    // method that checks the added user and updates the data base based on the newly created user
+
+
     @FXML
-    private void handleSignUp() {    	
+    private void togglePassword() {
+        boolean show = showPasswordCheck.isSelected();
+
+
+        passwordVisibleField.setVisible(show);
+        passwordVisibleField.setManaged(show);
+        passwordField.setVisible(!show);
+        passwordField.setManaged(!show);
+
+        confirmPasswordVisibleField.setVisible(show);
+        confirmPasswordVisibleField.setManaged(show);
+        confirmPasswordField.setVisible(!show);
+        confirmPasswordField.setManaged(!show);
+    }
+
+
+    @FXML
+    private void handleSignUp() {
+
         if (!passwordField.getText().equals(confirmPasswordField.getText())) {
-        	// output red warning in the ui
             System.out.println("Passwords do not match.");
             return;
         }
-        
-        // removed one of the parameter since username is automatically set as the first name (for now)
+
         User newUser = new User(
-        	emailField.getText(),
+            emailField.getText(),
             firstNameField.getText(),
             middleNameField.getText(),
             lastNameField.getText(),
-            categoryField.getText(),
+            categoryCombo.getValue(),  
             passwordField.getText()
         );
-        
-        // update userList through SceneManager
+
         SceneManager.getDataAccess().addUser(newUser);
-        
-        // console prints just for checking
+
         System.out.println("Account created!");
-        System.out.println("Name: " + newUser.getFirstName() + " " +
-        							  newUser.getMiddleName() + " " +
-        							  newUser.getLastName());
-        System.out.println("Email: " + newUser.getEmailAddress());
-        System.out.println("Password: " + newUser.getPassword());
-        
-        // go back to sign in page
+
         SceneManager.switchTo("/application/ui/sign_in.fxml");
     }
 }
